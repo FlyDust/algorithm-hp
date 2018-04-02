eb72785e18d4013e5ba9a76bfdf70fe846860741
FlyDust <1214617849@qq.com>
2018-04-02 18:35:01 +0800

    快速排序，添加头文件

--- src/sort/sort.h
+++ src/sort/sort.h
@@ -1,8 +1,18 @@
 #ifndef SORT_H
 #define SORT_H
 
-void insertion_sort(int *a, int length); 
+//插入排序O(n^2)
+void insertion_sort(int *a, int length);
+
+//归并排序O(nlgn)(非原址排序)
 void merge_sort(int *a, int low, int high);
+
+//堆排序O(nlgn)
 void heap_sort(int *a, int length);
+
+//快速排序O(n^2)~O(nlgn)
+void quick_sort(int *a, int low, int high);
+void randomized_quick_sort(int *a, int low, int high);	//随机化
+void tail_recursive_quick_sort(int *a, int low, int high);	//尾递归
 
 #endif
